79b26c0bf05795a42e0b850ff087ac48917fd1db
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2021-07-23 21:32:00 -0300

    ignews

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import Head from 'next/head'
+import styles from './home.module.scss'
+import { SubscribeButton } from '../components/SubscribeButton/index'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Ínicio | Ig.News</title>
+        <title>Home | Ig.News</title>
       </Head>
-      <h1>
-        Conteúdo
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton/>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
     </>
   )
 }
