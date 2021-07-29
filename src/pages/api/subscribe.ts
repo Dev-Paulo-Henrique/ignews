1a38449b64d3a3b48d8d51dbb37766ca1ab81119
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2021-07-29 19:08:13 -0300

    ignews

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,14 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { stripe } from "../../../services/stripe";
+import { stripe } from "../../services/stripe";
 import { getSession } from 'next-auth/client'
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default async(req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   if(req.method === 'POST'){
     const session = await getSession({req})
 
     const stripeCustomer = await stripe.customers.create({
-      email: session.user.email
+      email: session.user.email,
     })
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
@@ -16,18 +16,18 @@ export default async(req: NextApiRequest, res: NextApiResponse) => {
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
-        {price: 'price_1JHYaTCKYyhjp8iTpkhl1YQ8', quantity: 1}
+        {price: 'price_1JHYaTCKYyhjp8iTpkhl1YQ8',
+         quantity: 1},
       ],
       mode: 'subscription',
       allow_promotion_codes: true,
       success_url: process.env.STRIPE_SUCCESS_URL,
       cancel_url: process.env.STRIPE_CANCEL_URL,
     })
-
+    
     return res.status(200).json({sessionId: stripeCheckoutSession.id})
   }else{
     res.setHeader('Allow', 'POST')
     res.status(405).end('Method not allowed')
-    console.log(req)
   }
 }
