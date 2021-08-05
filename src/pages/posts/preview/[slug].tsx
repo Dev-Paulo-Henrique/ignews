0461166abcbca2f9cb98ccc4cc7c254e1343e135
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2021-08-05 16:49:09 -0300

    ignews

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next"
 import { useSession } from "next-auth/client"
 import Head from "next/head"
 import { RichText } from "prismic-dom"
@@ -52,7 +52,7 @@ export default function PostPreview({ post }: PostPreviewProps){
   )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths= async() => {
   return {
     paths: [],
     fallback: 'blocking'
@@ -77,6 +77,9 @@ export const getStaticProps: GetStaticProps = async({  params }) => {
   })}
 
   return{
-    props: {post,}
+    props: {
+      post,
+    },
+    redirect: 60 * 30 //30min
   }
 }
